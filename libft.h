ef13f7eb37430dc18aeaaf9b2e27e3c1c7c1931e
Karissa Sticks <ksticks@ga-p3.21-school.ru>
2019-10-14 19:47:18 +0300

    fix ftoa for extremely large numbers

--- libft.h
+++ libft.h
@@ -88,6 +88,7 @@ char				*ft_strnstr(const char *haystack,
 								const char *needle, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+char				*ft_strrev(char *str);
 int					ft_atoi(const char *str);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
--- src/format/ft_ftoa.c
+++ src/format/ft_ftoa.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
+
+static unsigned long long mod(long double a, int b)
+{
+	unsigned long long c;
+
+	c = (ULONG_LONG_MAX / b) * b;
+	a -= ((unsigned long long int)(a / c)) * c;
+	return (((unsigned long long)a) % b);
+}
 
 char *ft_ftoa_buf(char *s, long double n, int decimals)
 {
 	char *ptr;
 	int base;
+	long double m;
 
 	base = 10;
 	ptr = s;
@@ -14,14 +25,21 @@ char *ft_ftoa_buf(char *s, long double n, int decimals)
 		n *= -1;
 	}
 	n += 5 * ft_fpow(base, -(decimals + 1));
-	ptr += ft_strlen(ft_ultoa_buf(ptr, (long int)n, base));
+	m = n;
+	while (m >= 1.)
+	{
+		*ptr++ = '0' + (int)mod(m, base);
+		m /= base;
+	}
+	*ptr = 0;
+	ft_strrev(*s == '-' ? s + 1 : s);
 	if (!decimals)
 		return (s);
 	*ptr++ = '.';
 	while (decimals--)
 	{
 		n *= base;
-		*ptr++ = '0' + (int)((ulong)n % base);
+		*ptr++ = '0' + (int)(mod(n, base));
 	}
 	*ptr = 0;
 	return (s);
