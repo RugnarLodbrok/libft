b215fc6d3460700ad73a0abfdc91314944e3274a
Karissa Sticks <ksticks@ox-f5.21-school.ru>
2019-11-15 18:49:26 +0300

    norme

--- src/array.c
+++ src/array.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksticks <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 15:44:13 by rpoetess          #+#    #+#             */
+/*   Updated: 2019/11/08 15:44:14 by rpoetess         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void t_array_init(t_array *a, size_t item_size)
+void		t_array_init(t_array *a, size_t item_size)
 {
 	a->size = 4;
 	a->item_size = item_size;
@@ -8,23 +20,23 @@ void t_array_init(t_array *a, size_t item_size)
 	a->data = malloc(a->size * a->item_size);
 }
 
-void *t_array_get(t_array *a, int i)
+void		*t_array_get(t_array *a, int i)
 {
 	if (i >= a->count)
 		return (0);
 	return (a->data + i * a->item_size);
 }
 
-static void *t_array_extend(t_array *a)
+static void	*t_array_extend(t_array *a)
 {
 	a->size *= 2;
 	a->data = ft_realloc(a->data,
-						 a->size * a->item_size,
-						 a->count * a->item_size);
+			a->size * a->item_size,
+			a->count * a->item_size);
 	return (a->data);
 }
 
-void *t_array_push(t_array *a, void *content)
+void		*t_array_push(t_array *a, void *content)
 {
 	if (a->count == a->size - 1)
 		if (!t_array_extend(a))
@@ -34,7 +46,7 @@ void *t_array_push(t_array *a, void *content)
 	return (a->data + a->item_size * (a->count - 1));
 }
 
-void t_array_del(t_array *a)
+void		t_array_del(t_array *a)
 {
 	free(a->data);
 }
