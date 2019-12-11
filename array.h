907ad1d983e321366fab195d55f4267d7cd91cbc
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2019-12-11 15:52:02 +0300

    arrayp

--- array.h
+++ array.h
@@ -16,6 +16,13 @@ typedef struct	s_arrayi
 	int		*data;
 }				t_arrayi;
 
+typedef struct	s_arrayp
+{
+	size_t	count;
+	size_t	size;
+	void	**data;
+}				t_arrayp;
+
 void	t_array_init(t_array *a, size_t item_size);
 void	*t_array_get(t_array *a, int i);
 void	*t_array_push(t_array *a, void *content);
@@ -25,5 +32,10 @@ void	t_arrayi_init(t_arrayi *a);
 int		t_arrayi_get(t_arrayi *a, int i, int default_v);
 int		t_arrayi_push(t_arrayi *a, int v);
 void	t_arrayi_del(t_arrayi *a);
+
+void	t_arrayp_init(t_arrayp *a);
+void	*t_arrayp_get(t_arrayp *a, int i, void *default_v);
+int		t_arrayp_push(t_arrayp *a, void *v);
+void	t_arrayp_del(t_arrayp *a);
 
 #endif
---
+++ src/array/arrayp.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arrayp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksticks <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/26 18:07:27 by ksticks           #+#    #+#             */
+/*   Updated: 2019/11/26 18:07:29 by ksticks          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	t_arrayp_init(t_arrayp *a)
+{
+	a->size = 4;
+	a->count = 0;
+	a->data = malloc(a->size * sizeof(void*));
+}
+
+void	*t_arrayp_get(t_arrayp *a, int i, void *default_v)
+{
+	if ((uint)i >= a->count)
+		return (default_v);
+	return a->data[i];
+}
+
+static void	*t_arrayp_extend(t_arrayp *a)
+{
+	a->size *= 2;
+	a->data = ft_realloc(a->data,
+						 a->size * sizeof(void*),
+						 a->count * sizeof(void*));
+	return (a->data);
+}
+
+int			t_arrayp_push(t_arrayp *a, void *v)
+{
+	if (a->count == a->size - 1)
+		if (!t_arrayp_extend(a))
+			return (0);
+	a->data[a->count] = v;
+	return ++a->count;
+}
+
+void		t_arrayp_del(t_arrayp *a)
+{
+	free(a->data);
+}
